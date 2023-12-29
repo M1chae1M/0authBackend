49c63578c4705d8126d4dc872f29e4588888d894
M1chae1M <michalmaru10@gmail.com>
2023-12-29 11:17:28 +0100

    works with new insert?

--- all/src/database/insert/index.js
+++ all/src/database/insert/index.js
@@ -1,16 +1,26 @@
 import baza from ".."
 
-const fields=['id','name','age','email','city','country','occupation','salary']
-
 export default async function insertQuery(req,res){
     const {data,offset_data}=req.body??{}
     const {limit,page}=offset_data??{}
 
     const db_query_imitation=await baza.selected_page('display_data',page,limit)
     const query_req=await baza.insert('display_data', data)
+    
+    // const fields=['id','name','age','email','city','country','occupation','salary']
+    // const newRecord=Object?.fromEntries?.(fields?.map(x=>[x, '']))??{};
+    // const newRecord = fields?.reduce((acc,field)=>({...acc,[field]:''}),{});
+    const newRecord={
+        id:'',
+        name:'',
+        age:'',
+        email:'',
+        city:'',
+        country:'',
+        occupation:'',
+        salary:''
+    };
 
-    // const newRecord=Object?.fromEntries?.(fields?.map(x=>[x, '']))??[];
-    const newRecord=[]
     db_query_imitation.push({ ...newRecord, ...data, id:null });
 
     res.json({query_req, db_query_imitation})
