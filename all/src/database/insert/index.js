a9d890089c3223140a8174a40297b742bc4ebce5
M1chae1M <michalmaru10@gmail.com>
2023-12-29 15:40:50 +0100

    insert bug fixed

--- all/src/database/insert/index.js
+++ all/src/database/insert/index.js
@@ -3,9 +3,9 @@ import insert_imitation from "./imitation"
 
 export default async function insertQuery(req,res){
     const {data,offset_data}=req.body??{}
-
-    const query_req=await baza.insert('display_data', data)
+    
     const db_query_imitation=await insert_imitation(data,offset_data)
+    const query_req=await baza.insert('display_data', data)
 
     res.json({query_req, db_query_imitation})
 }
